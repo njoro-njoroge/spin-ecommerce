7b7646afb7334999116424c8d7664b5ddd8ce1d0
Njoroge1 <njoro.njoroge02@gmail.com>
2023-04-30 23:09:44 +0300

    created a factory instance. And pass it to ViewModelProvider.

--- app/src/main/java/com/njoro/spin/ui/productdetails/ProductDetailsFragment.kt
+++ app/src/main/java/com/njoro/spin/ui/productdetails/ProductDetailsFragment.kt
@@ -1,12 +1,13 @@
 package com.njoro.spin.ui.productdetails
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.njoro.spin.R
+import com.njoro.spin.databinding.FragmentProductDetailsBinding
 
 class ProductDetailsFragment : Fragment() {
 
@@ -14,19 +15,26 @@ class ProductDetailsFragment : Fragment() {
         fun newInstance() = ProductDetailsFragment()
     }
 
-    private lateinit var viewModel: ProductDetailsViewModel
 
+
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_product_details, container, false)
+        val application = requireNotNull(activity).application
+        val binding = FragmentProductDetailsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner= this
+        val products = ProductDetailsFragmentArgs.fromBundle(arguments!!).selectedProducts
+
+        val viewModelFactory = ProductDetailViewModelFactory(products, application)
+
+          binding.viewModel = ViewModelProvider(this, viewModelFactory).get(ProductDetailsViewModel::class.java)
+
+        return binding.root
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProductDetailsViewModel::class.java)
 
-    }
 
 }

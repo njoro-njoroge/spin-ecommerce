8dde3288380a2263aceba0fa2deadad0d3879e8d
Njoroge1 <njoro.njoroge02@gmail.com>
2023-05-01 18:30:24 +0300

    add bottom navigation and dataBinding

--- app/src/main/java/com/njoro/spin/MainActivity.kt
+++ app/src/main/java/com/njoro/spin/MainActivity.kt
@@ -1,36 +1,76 @@
 package com.njoro.spin
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.njoro.spin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//navController=Navigation.findNavController(this,R.id.nav_host_fragment)
+//        setupActionBarWithNavController(binding.navView.)
+
+        navView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.dashboardFragment,
+                R.id.cartFragment,
+                R.id.loginFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+//        showBottomNav()
+
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
-//
 //        binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
-//
-//
-//        val navView: BottomNavigationView = binding.navView
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+//        navController= Navigation.findNavController(this,R.id.nav_host_fragment)
+//        setupWithNavController
+//                val navView: BottomNavigationView = binding.navView
+//        val navController = findNavController(R.id.nav_host_fragment)
 //        // Passing each menu ID as a set of Ids because each
 //        // menu should be considered as top level destinations.
 //        val appBarConfiguration = AppBarConfiguration(
 //            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.fragment_login
+//                R.id.homeFragment, R.id.navigation_dashboard, R.id.fragment_login
 //            )
 //        )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
 
 
-//    }
+    fun hideBottomNav() {
+        navView.visibility = View.GONE
+    }
+
+    fun showBottomNav() {
+        navView.visibility = View.VISIBLE
     }
 }

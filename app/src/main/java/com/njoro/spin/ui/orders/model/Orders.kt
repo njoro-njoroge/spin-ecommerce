1544839f3b2941611971d89182bac91fa2e25502
Njoroge1 <njoro.njoroge02@gmail.com>
2023-05-08 01:01:29 +0300

    Added orderItem data class

--- app/src/main/java/com/njoro/spin/ui/orders/model/Orders.kt
+++ app/src/main/java/com/njoro/spin/ui/orders/model/Orders.kt
@@ -1,6 +1,8 @@
 package com.njoro.spin.ui.orders.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
 
 data class OrderResponse(
@@ -10,14 +12,34 @@ data class OrderResponse(
    val data: List<OrdersModel>
 )
 
+@Parcelize
 data class OrdersModel(
     val orderId: String,
     val orderCost: Double,
     val orderDate: String,
     val orderStatus: String
-)
+):Parcelable
 
 data class UserOrders(
     @Json(name = "userId")
     val userId: String
+)
+
+data class OrderItemsResponse(
+    val status: Boolean,
+    val message:String,
+    @Json(name="details")
+    val data: List<OrderItems>
+)
+
+data class OrderItems(
+    val itemId: String,
+    val itemName: String,
+    val itemPrice: String,
+    val itemQuantity: Double,
+    val subTotal: Double
+)
+data class OrderItemsById(
+    @Json(name = "orderId")
+    val orderId: String
 )

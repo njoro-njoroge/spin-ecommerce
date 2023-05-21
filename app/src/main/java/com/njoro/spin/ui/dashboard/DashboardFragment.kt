3fb9d99f68a35dede1016c2e40fb84fd8ef61506
Njoroge1 <njoro.njoroge02@gmail.com>
2023-05-21 12:43:03 +0300

    Add new fuctions

--- app/src/main/java/com/njoro/spin/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/njoro/spin/ui/dashboard/DashboardFragment.kt
@@ -1,29 +1,26 @@
 package com.njoro.spin.ui.dashboard
 
 import android.R
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.njoro.ecommerce.utils.IPreferenceHelper
-import com.njoro.ecommerce.utils.PreferenceManager
+import com.njoro.ecommerce.utils.SessionManager
+import com.njoro.spin.MainActivity
 import com.njoro.spin.databinding.FragmentDashboardBinding
-import java.lang.reflect.Array.get
 
 
 class DashboardFragment : Fragment() {
 
-    private val pref: IPreferenceHelper by lazy {
-        PreferenceManager(requireContext())
-    }
-
+    //    private lateinit var authViewModel: AuthViewModel
+    private lateinit var sessionManager: SessionManager
     private var _binding: FragmentDashboardBinding? = null
 
 
@@ -36,50 +33,80 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+//        val dashboardViewModel =
+//            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+//
+        sessionManager = SessionManager(requireContext())
+//
+        listItems()
+        return root
+    }
 
+    private fun listItems() {
         var arrayAdapter: ArrayAdapter<String>
         val userActions = arrayOf(
             "Edit profile", "Orders", "Feedback",
             "Cart", "Logout"
         )
         val listView: ListView = binding.userlist
-         arrayAdapter = ArrayAdapter(inflater.context,
-            R.layout.simple_list_item_1, userActions)
+        arrayAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_list_item_1, userActions
+        )
         listView.adapter = arrayAdapter
-
-        listView.setOnItemClickListener(){
-                adapterView, view, position, id ->
+        listView.setOnItemClickListener{ adapterView, view, position, id ->
 //            val itemAtPos = adapterView.getItemAtPosition(position)
             val itemIdAtPos = adapterView.getItemIdAtPosition(position)
             val id = itemIdAtPos
 
-            if(pref.getUsername().isEmpty()){
-                Toast.makeText(context,"Please login",Toast.LENGTH_SHORT).show()
+            if (!sessionManager.isLoggedIn()) {
+                Toast.makeText(context, "Please login", Toast.LENGTH_SHORT).show()
                 return@setOnItemClickListener
             }
 
-            if(position == 0){
+            if (position == 0) {
                 gotToProfile()
             }
-            if(position == 1){
+            if (position == 1) {
                 goToOrders()
             }
+            if (position == 4) {
+                logoutAlert()
+            }
         }
-        return root
     }
 
-    private fun gotToProfile(){
-        this.findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToProfileFragment())
+    private fun gotToProfile() {
+//        val action = EmployeeDashboardDirections.actionEmployeeDashboardToProfileFragment()
+//        findNavController().navigate(action)
     }
 
-    private fun goToOrders(){
-        this.findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToOrdersFragment())
+    private fun goToOrders() {
+//        this.findNavController()
+//            .navigate(DashboardFragmentDirections.actionDashboardFragmentToOrdersFragment())
+    }
+
+    private fun logoutAlert() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Logout")
+        builder.setMessage("Are you sure you want to logout?")
+        builder.setNegativeButton("No", null)
+        builder.setPositiveButton("Yes") { dialog, _ ->
+            logoutUser()
+            dialog.dismiss()
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun logoutUser() {
+        sessionManager.logout()
+        (activity as MainActivity).checkUserSession()
+        Toast.makeText(context, "You have logged out", Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroyView() {

9205e8c881221a73fa85a94dc26c6ef532e30642
Njoroge1 <njoro.njoroge02@gmail.com>
2023-05-21 12:29:01 +0300

    Modifiled navigation function and added a function to check user session

--- app/src/main/java/com/njoro/spin/MainActivity.kt
+++ app/src/main/java/com/njoro/spin/MainActivity.kt
@@ -5,14 +5,21 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import com.njoro.ecommerce.utils.SessionManager
 import com.njoro.spin.databinding.ActivityMainBinding
+import com.njoro.spin.ui.auth.login.AuthViewModel
+import com.njoro.spin.ui.home.HomeFragmentDirections
 import com.njoro.spin.utils.ConnectivityObserver
 import com.njoro.spin.utils.NetworkConnectivityObserver
 import kotlinx.coroutines.flow.launchIn
@@ -24,8 +31,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var connectivityObserver: ConnectivityObserver
     private lateinit var binding: ActivityMainBinding
     private lateinit var navView: BottomNavigationView
-    private lateinit var navController: NavController
-
+    private lateinit var viewModel: AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,45 +40,73 @@ class MainActivity : AppCompatActivity() {
 //navController=Navigation.findNavController(this,R.id.nav_host_fragment)
 //        setupActionBarWithNavController(binding.navView.)
 
+        viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
         connectivityObserver = NetworkConnectivityObserver(applicationContext)
         connectivityObserver.observe().onEach {
             println("Status is $it")
-            if(it.toString() =="Online"){
+            if (it.toString() == "Online") {
 
                 Toast.makeText(this, "Online", Toast.LENGTH_SHORT).show()
-            }else{
+            } else {
                 Toast.makeText(this, "Network $it", Toast.LENGTH_SHORT).show()
             }
 
         }.launchIn(lifecycleScope)
 
         navView = binding.navView
-        navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.dashboardFragment,
-                R.id.cartFragment,
-                R.id.loginFragment
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
-//        navView.menu.findItem(R.id.loginFragment).isVisible = false
-
-
+        bottomNavigation()
+        checkUserSession()
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home) {
+        if (item.itemId == android.R.id.home) {
             onBackPressedDispatcher.onBackPressed()
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun bottomNavigation(){
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+
+        navController?.let { controller ->
+            bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.homeFragment -> {
+                        controller.navigate(R.id.homeFragment)
+                        true
+                    }
+                    R.id.dashboardFragment -> {
+                        controller.navigate(R.id.dashboardFragment)
+                        true
+                    }
+                    R.id.loginFragment->{
+                        controller.navigate(R.id.loginFragment)
+                        true
+                    }
+                    R.id.cartFragment->{
+                        controller.navigate(R.id.cartFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
+
+
+     fun checkUserSession() {
+         viewModel.loginState()
+        viewModel.isUserLoggedIn.observe(this) { userLogin ->
+            navView.menu.findItem(R.id.loginFragment).isVisible = !userLogin
+
+        }
+    }
+
+
 
     fun hideBottomNav() {
         navView.visibility = View.GONE
